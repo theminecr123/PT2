46fcabb4bb979b056a9f226f020842a6eb52b8d0
Dong Trieu <tranluudongtrieu@gmail.com>
2024-08-16 16:20:29 +0700

    optimized

--- PT2.php
+++ PT2.php
@@ -83,43 +83,41 @@
         </form>
 
         <?php
+        function giaiPT2($a, $b, $c) {
+            if ($a == 0) {
+                if ($b == 0) {
+                    return ($c == 0) ? "Phương trình vô số nghiệm." : "Phương trình vô nghiệm.";
+                } else {
+                    $x = -$c / $b;
+                    return "Phương trình có một nghiệm: x = $x";
+                }
+            } else {
+                $delta = $b * $b - 4 * $a * $c;
+
+                if ($delta < 0) {
+                    return "Phương trình vô nghiệm.";
+                } elseif ($delta == 0) {
+                    $x = -$b / (2 * $a);
+                    return "Phương trình có nghiệm kép:<br>x1 = x2 = $x";
+                } else {
+                    $x1 = (-$b + sqrt($delta)) / (2 * $a);
+                    $x2 = (-$b - sqrt($delta)) / (2 * $a);
+                    return "Phương trình có hai nghiệm phân biệt:<br>x1 = $x1<br>x2 = $x2";
+                }
+            }
+        }
+
         if (isset($_POST['solve'])) {
             $a = $_POST['a'];
             $b = $_POST['b'];
             $c = $_POST['c'];
 
             echo '<div class="result">';
-            if ($a == 0) {
-                if ($b == 0) {
-                    if ($c == 0) {
-                        echo "Phương trình vô số nghiệm.";
-                    } else {
-                        echo "Phương trình vô nghiệm.";
-                    }
-                } else {
-                    $x = -$c / $b;
-                    echo "Phương trình có một nghiệm: x = $x";
-                }
-            } else {
-                $delta = $b * $b - 4 * $a * $c;
-
-                if ($delta < 0) {
-                    echo "Phương trình vô nghiệm.";
-                } elseif ($delta == 0) {
-                    $x = -$b / (2 * $a);
-                    echo "Phương trình có nghiệm kép:<br>";
-                    echo "x1 = x2 = $x";
-                } else {
-                    $x1 = (-$b + sqrt($delta)) / (2 * $a);
-                    $x2 = (-$b - sqrt($delta)) / (2 * $a);
-                    echo "Phương trình có hai nghiệm phân biệt:<br>";
-                    echo "x1 = $x1<br>";
-                    echo "x2 = $x2";
-                }
-            }
+            echo giaiPT2($a, $b, $c);
             echo '</div>';
         }
         ?>
+
     </div>
 </body>
 </html>
